aa39b49b5ce89e38224df9ff1da4b8a9e3469693
NgwangShalom <ngwangshalom956@gmail.com>
2023-05-24 02:09:04 +0100

    Started Subscription Manager

--- app/Core/SubscriptionManager.php
+++ app/Core/SubscriptionManager.php
@@ -1,3 +1,5 @@
+
+<?php
 // company: PulseStream
 // Developed by: Ngwang Shalom
 // Location: Cameroon/Bamenda
@@ -9,3 +11,63 @@
 //
 //
 //
+class SubscriptionManager {
+    private $db;
+
+    public function __construct() {
+        // Initialize the database connection
+        $this->db = new PDO("mysql:host=your_database_host;dbname=your_database_name", "your_username", "your_password");
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function createPayment($userId, $amount) {
+        // Create a new payment record in the database
+        $query = "INSERT INTO payments (user_id, amount, created_at) VALUES (:user_id, :amount, NOW())";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':user_id', $userId);
+        $stmt->bindParam(':amount', $amount);
+        $stmt->execute();
+
+        // Return the payment ID
+        return $this->db->lastInsertId();
+    }
+
+    public function trackPaymentsPerMonth() {
+        // Get the total payments per month for the current year
+        $query = "SELECT MONTH(created_at) as month, SUM(amount) as total_amount FROM payments WHERE YEAR(created_at) = YEAR(CURDATE()) GROUP BY MONTH(created_at)";
+        $stmt = $this->db->query($query);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Return the payment per month data
+        return $results;
+    }
+
+    public function sendPaymentExpirationNotifications() {
+        // Get the users whose payment has expired
+        $query = "SELECT u.email FROM users u LEFT JOIN payments p ON u.id = p.user_id WHERE p.created_at < DATE_SUB(NOW(), INTERVAL 1 YEAR) OR p.id IS NULL";
+        $stmt = $this->db->query($query);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Send notification emails to the users
+        foreach ($results as $row) {
+            $email = $row['email'];
+            // Send the email notification to $email
+            // ...
+        }
+    }
+}
+
+// Usage example:
+$subscriptionManager = new SubscriptionManager();
+
+// Create a payment for a user
+$paymentId = $subscriptionManager->createPayment(123, 50.00);
+
+// Track payments per month
+$paymentPerMonth = $subscriptionManager->trackPaymentsPerMonth();
+print_r($paymentPerMonth);
+
+// Send payment expiration notifications
+$subscriptionManager->sendPaymentExpirationNotifications();
+
+?>
